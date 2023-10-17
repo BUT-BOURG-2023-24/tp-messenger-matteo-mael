e45bb0df56bd34341cb164d88a9d9c03889be8e9
Mattéo Miguelez <matteo.miguelez@gmail.com>
2023-10-17 12:22:56 +0200

    feat: init conversationController

---
+++ src/controller/ConversationController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+
+async function getConversationWithParticipants(req: Request, res: Response) {}
+
+async function getAllConversationsForUser(req: Request, res: Response) {}
+
+async function getConversationById(req: Request, res: Response) {}
+
+async function createConversation(req: Request, res: Response) {}
+
+async function addMessageToConversation(req: Request, res: Response) {}
+
+async function setConversationSeenForUserAndMessage(
+  req: Request,
+  res: Response
+) {}
+
+async function deleteConversation(req: Request, res: Response) {}
+
+module.exports = {
+  getConversationWithParticipants,
+  getAllConversationsForUser,
+  getConversationById,
+  createConversation,
+  addMessageToConversation,
+  setConversationSeenForUserAndMessage,
+  deleteConversation,
+};
